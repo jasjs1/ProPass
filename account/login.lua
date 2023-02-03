7234ec2d84de0c99f3e032216788568af6776141
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-03 07:31:40 -0600

    added signup with 2fa and changed variables

--- account/login.lua
+++ account/login.lua
@@ -1,19 +1,41 @@
-local function login()
+print("Your email:")
+local email = io.read()
 
-    io.write("Email: ")
-    local loginEmail = io.read()
+print("Your password:")
+local signupPassword = io.read()
 
-    io.write("Password: ")
-    local loginPassword = io.read()
+local twoFactorFile = "2FactorCode.txt"
+local twoFactorCode = nil
 
+if io.open(twoFactorFile, "r") then
+    print("Your 2 Factor Authentication code:")
+    local input = io.read()
+
+    local file = io.open(twoFactorFile, "r")
+    twoFactorCode = file:read()
+    file:close()
+
+    if input ~= twoFactorCode  then
+        print("Incorrect 2 Factor Authentication code.")
+        return
+    end
 end
-login()
 
-local function writeLogFile()
+local accountDataFile = "userProvidedAccountData.txt"
 
-    local file = io.open("loginLog.txt")
+if io.open(accountDataFile, "r") then
+    local file = io.open(accountDataFile, "r")
+    local data = file:read("*all")
+    file:close()
 
-    file:write(loginEmail)
-    file:write(loginPassword)
+    local emailLine = string.match(data, "Email:([^\r\n]+)")
+    local passwordLine = string.match(data, "Password:([^\r\n]+)")
+
+    if emailLine == email and passwordLine == password then
+        print("Login successful.")
+    else
+        print("Incorrect email or password.")
+    end
+else
+    print("No account data found for this email.")
 end
-writeLogFile()
