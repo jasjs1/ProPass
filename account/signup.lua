1f3ed9b385e81219ce76ec6a8aa0458b0eb648b2
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-03 07:21:58 -0600

    pr: jSagvold-2fa-impmentation-1

--- account/signup.lua
+++ account/signup.lua
@@ -1,6 +1,5 @@
-
 print("Name: ")
-local name = io.read()
+local userName = io.read()
 
 print("Email:")
 local email = io.read()
@@ -11,7 +10,26 @@ local userAge = io.read()
 print("Password:")
 local signupPassword = io.read()
 
+print("Do you want to have 2 Factor Authentication on?")
 
+local input = io.read()
+
+local function enable2FA()
+    file = io.open("2FactorCode.txt", "w")
+
+io.output():write("Your 2FA code: ")
+twoFactorCode = io.input():read()
+
+file:write(twoFactorCode)
+file:close()
+
+end
+
+if input == "yes" then
+    
+    enable2FA()
+
+end
 
 
 local function fetchProvidedData()
@@ -22,9 +40,9 @@ local function fetchProvidedData()
 
     if input == "yes" then
 
-        local file = io.open("userProvidedAccountData.txt")
+        local file = io.open("userProvidedAccountData.txt", "w")
 
-        file:write(name)
+        file:write(userName)
         file:write(email)
         file:write(userAge)
         file:write(signupPassword)
@@ -40,4 +58,3 @@ local function fetchProvidedData()
     end
 end
 fetchProvidedData()
-
