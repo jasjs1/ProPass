fdbb24a017c2f3f68c89b126fdd87c53a2f2b302
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-04 14:50:03 -0600

    format change

--- .apps/passwordGenerator.lua
+++ .apps/passwordGenerator.lua
@@ -8,9 +8,20 @@ local function generatePassword(length)
     return password
 end
 
+print("")
+print("Enter desired password length (8-40 characters):")
+local length = tonumber(io.read())
+while length < 8 or length > 40 do
+    print("Invalid length. Enter a length between 8 and 40:")
+    length = tonumber(io.read())
+end
 
 
-local password = generatePassword(15)
+print("Password for: ")
+local passwordFor = io.read()
+
+local password = generatePassword(length)
+print("")
 print("Your generated password is: " .. password)
 print("Type 'save' to save your password!")
 
@@ -21,18 +32,28 @@ if input == "save" then
     local file = io.open(filename, "w")
 
     print("Password file generated! Check the explorer!")
-    file:write(password)
+    file:write(password, ", ")
+    file:write(passwordFor)
     file:close()
 end
 
-
-
 while true do
-
     local input = io.read()
 
     if input == "generate new" then
-        generatePassword()
+        password = generatePassword(length)
+        print("Your generated password is: " .. password)
+        print("Type 'save' to save your password!")
 
+        local input = io.read()
+
+        if input == "save" then
+            local filename = "Saved Password_" .. os.date("%M-%d_%y_%H_%M")
+            local file = io.open(filename, "w")
+
+            print("Password file generated! Check the explorer!")
+            file:write(password)
+            file:close()
+        end
     end
 end
