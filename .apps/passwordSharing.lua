c7c06261bfa120927708977bafcea48cfc1c080b
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-05 09:31:10 -0600

    MVP, password sharing

---
+++ .apps/passwordSharing.lua
@@ -0,0 +1,26 @@
+local function getUserInput()
+    print("Username/email:")
+    local username = io.read()
+
+    print("Password: ")
+    local password = io.read()
+
+    print("Password for:")
+    local passwordFor = io.read()
+    
+    return username, password, passwordFor
+end
+
+local username, password, passwordFor = getUserInput()
+
+local function writeToFile()
+    local file = io.open("SharePassword.txt", "w")
+
+    file:write(username .. "\n")
+    file:write(password .. "\n")
+    file:write(passwordFor .. "\n")
+
+    file:close()
+end
+
+writeToFile()
