497add66c99d5df5451b9eda6a207b927bbd597b
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-06 07:04:21 -0600

    Update passwordSharing.lua

--- .apps/passwordSharing.lua
+++ .apps/passwordSharing.lua
@@ -1,29 +1,46 @@
 local function getUserInput()
 
+
     print("")
-    print("Username/email:")
-    local username = io.read()
+    print("Login email: ")
+    local loginEmail = io.read()
+
 
     print("Password: ")
     local password = io.read()
 
-    print("Password for:")
-    local passwordFor = io.read()
-    
-    return username, password, passwordFor
+
+    print("Place that this password is used")
+    local siteThatPasswordIsUsed = io.read()
+
+
+    print("Recipient of the password (email, name, phone number)")
+    local recipientOfPassword = io.read()
+
+    print("Term of use for recipiant (ProPass can not manage that)")
+    local termOfPasswordUse = io.read()
+
+
+    return loginEmail, password, siteThatPasswordIsUsed, recipientOfPassword, termOfPasswordUse
+
 
 end
 
-local username, password, passwordFor = getUserInput()
+local loginEmail, password, siteThatPasswordIsUsed, recipientOfPassword = getUserInput()
 
 local function writeToFile()
-    local file = io.open("SharePassword.txt", "w")
 
-    file:write(username .. "\n")
-    file:write(password .. "\n")
-    file:write(passwordFor .. "\n")
+    local file = io.open("sharedPassword.txt", "w")
+    local fileName=  file
+
+    file:write("Login Email: " .. loginEmail .. "\n")
+    file:write("Password: " .. password .. "\n")
+    file:write("Site that password is used on: " .. siteThatPasswordIsUsed .. "\n")
+    file:write("Recipiant of password: " .. recipientOfPassword .. "\n")
 
     file:close()
 
 end
-writeToFile()
+
+print("File has been created! Check the file explorer!")
+print("If you want to do another please run the file again!")
