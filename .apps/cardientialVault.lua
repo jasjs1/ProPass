14e7dc75e5cb19786a2fc6348697607bee429e12
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-07 17:45:30 -0600

    code rewrite, better UX experience

--- .apps/cardientialVault.lua
+++ .apps/cardientialVault.lua
@@ -1,36 +1,33 @@
-local providedCredentials, passwordUse, hostSecurity
-
 local function fetchProvidedData()
-    print("Enter the credentials that you want to save:")
-    providedCredentials = io.read()
 
-    print("Website/app name that this password is used on:")
-    passwordUse = io.read()
+    print("Login email:")
+    local loginEmail = io.read()
 
-    print("Host security:")
-    hostSecurity = io.read()
+    print("Password: ")
+    local password = io.read()
+
+    print("Website/app name: ")
+    local websiteHost = io.read()
+
+    return loginEmail, password, websiteHost
 end
 
-fetchProvidedData()
+local loginEmail, password, websiteHost = fetchProvidedData()
 
-local function validateDetailsCreateFile()
-    print("Validate that the information that you typed in is correct: ")
-    print("")
-    print(providedCredentials)
-    print(passwordUse)
-    print(hostSecurity)
+local function writeFile()
 
     print("")
     print("Type 'yes' if you want to create a vault file with the information above?")
 
     local input = io.read()
     if input == "yes" then
-        local file = io.open("savedCardentials.txt", "w")
-        file:write(providedCredentials .. "\n")
-        file:write(passwordUse .. "\n")
-        file:write(hostSecurity .. "\n")
+        local file = io.open("savedCredentials.txt", "w")
+
+        file:write("Login email:" .. loginEmail .. "\n")
+        file:write("Password: " .. password .. "\n")
+        file:write("Website/app name: " .. websiteHost .. "\n")
+
         file:close()
     end
 end
-
-validateDetailsCreateFile()
+writeFile()
