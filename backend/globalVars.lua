2e72ebe9c7309c2e9e37e37468bdc46da2a98f2f
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-09 07:10:37 -0600

    added errorMessage varible

--- backend/globalVars.lua
+++ backend/globalVars.lua
@@ -1,4 +1,5 @@
 local input = io.read()
 local password = ""
+local errorMessage = "There has been an error. Please try again." -- The error message is not used alot in this project. This may appear later
 
 -- There could possibly more global vars that will apper in this file later.
