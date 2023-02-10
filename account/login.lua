e478567ab49f64bf2b4565fedb53bdacb2201377
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-10 16:23:31 -0600

    removed 2FA to login

--- account/login.lua
+++ account/login.lua
@@ -4,38 +4,29 @@ local email = io.read()
 print("Your password:")
 local signupPassword = io.read()
 
-local twoFactorFile = "2FactorCode.txt"
-local twoFactorCode = nil
-
-if io.open(twoFactorFile, "r") then
-    print("Your 2 Factor Authentication code:")
-    local input = io.read()
-
-    local file = io.open(twoFactorFile, "r")
-    twoFactorCode = file:read()
-    file:close()
-
-    if input ~= twoFactorCode  then
-        print("Incorrect 2 Factor Authentication code.")
-        return
-    end
-end
 
 local accountDataFile = "userProvidedAccountData.txt"
 
 if io.open(accountDataFile, "r") then
+
     local file = io.open(accountDataFile, "r")
     local data = file:read("*all")
-    file:close()
+
+    file:close()    
 
     local emailLine = string.match(data, "Email:([^\r\n]+)")
     local passwordLine = string.match(data, "Password:([^\r\n]+)")
 
     if emailLine == email and passwordLine == password then
+        
         print("Login successful.")
+
     else
         print("Incorrect email or password.")
     end
+
 else
+
     print("No account data found for this email.")
+
 end
