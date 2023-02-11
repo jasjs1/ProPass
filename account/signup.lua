5015c8c6b6d0e2bd18e60969ab4ae43ec9c46b2a
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-10 19:38:06 -0600

    file name that was created changed, along with some other changes

--- account/signup.lua
+++ account/signup.lua
@@ -1,60 +1,71 @@
+print("")
 print("Name: ")
-local userName = io.read()
+local name = io.read()
 
 print("Email:")
 local email = io.read()
 
 print("Age")
-local userAge = io.read()
+local age = io.read()
 
 print("Password:")
 local signupPassword = io.read()
 
 print("Do you want to have 2 Factor Authentication on?")
+local TwoFactorOn = nil
 
 local input = io.read()
 
 local function enable2FA()
+
     file = io.open("2FactorCode.txt", "w")
 
-io.output():write("Your 2FA code: ")
-twoFactorCode = io.input():read()
+    io.output():write("Your 2FA code: ")
+    twoFactorCode = io.input():read()
 
-file:write(twoFactorCode)
-file:close()
-
-end
-
-if input == "yes" then
-    
-    enable2FA()
+    file:write(twoFactorCode)
+    file:close()
 
 end
 
+if input == "yes" then enable2FA() end
 
 local function fetchProvidedData()
 
-    print("type 'yes' to create a ProPass account.")
+    print("Vaidate that the information below is correct:")
+    print("")
+    print("Name: " .. name)
+    print("Email: " .. email)
+    print("Age: " .. age)
+    print("Password: " .. signupPassword)
+    -- print("2FA on: " .. TwofactorAuthentication)
+
+    print("Type 'yes' if the information below is correct.")
 
     local input = io.read()
 
     if input == "yes" then
 
-        local file = io.open("userProvidedAccountData.txt", "w")
+        local file = io.open("userAccountData.txt", "w")
 
-        file:write(userName)
-        file:write(email)
-        file:write(userAge)
-        file:write(signupPassword)
+        file:write("Name: " .. name .. "\n")
+        file:write("Email: " .. email .. "\n")
+        file:write("Age: " .. age .. "\n")
+        file:write("Password: " .. signupPassword .. "\n")
 
         file:close()
 
-        print("ProPass account created.")
+        print("ProPass account created! Welcome to a new was to get passwords!")
 
-    else 
+    else
         print("ProPass account was not created.")
 
+        if input == "no" then
+            print(
+                "ProPass was not created. This was caused by you inputting no to the validate information.")
 
+            -- getUserInput()
+        end
     end
 end
 fetchProvidedData()
