06a4db23526200539f1166165809c5ee324f0dcc
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-18 19:52:45 -0600

    file extension name change, format change

--- contributing.lua
+++
@@ -1,7 +0,0 @@
-
-## Contribution to this project:
-
-This is a 100% open source project. Contribtuions are all ways welcome!
-
-### Updates to the codebase
-If you would like to update/change any code in this codebase please fork this reposiotry. And when your change is made create a PR with the change. If the change is good, I will accept the PR and push it into the production in the codebase.
