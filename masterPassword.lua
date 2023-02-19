c36cd88a5941f1127ddcc8e6b85e6dd060bbcb3e
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-18 19:47:06 -0600

    deleted; not needed

--- masterPassword.lua
+++
@@ -1,8 +0,0 @@
-file = io.open("masterPassword.txt", "w")
-
-io.write("Master password: ")
-masterPassword = io.input():read()
-
-file:write(masterPassword)
-file:close()
-local storedMasterPasssword = masterPassword
