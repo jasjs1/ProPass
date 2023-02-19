cc4e85d6235fa80ffa8434004ad45de5c2af56df
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-18 20:14:43 -0600

    comment block change

--- .apps/passwordStrength/gradePasswordStrength.lua
+++ .apps/passwordStrength/gradePasswordStrength.lua
@@ -1,6 +1,6 @@
 --[[
     This file is graded on a grading scale of (A, B, C, D, F)
-    If you DONT want to have a letter grade scale go to the basicPasswordStrength.lua file, this file is weak - extremly strong scale.
+    If you DONT want to have a letter grade scale go to the basicPasswordStrength.lua file, this file is scale on weak - extremly strong scale.
 ]]
 
 function passwordStrength(password)
