caa71c741fe9bf7e7dd798181b6d58e683ff2dd0
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-20 08:22:10 -0600

    initial commit

---
+++ subscription/loginToSubscription.lua
@@ -0,0 +1,13 @@
+print("Email:")
+local subscriptionEmail = io.read()
+
+print("Password: ")
+local subscriptionPassword = io.read()
+
+local subscriptionEmail = true
+local subscriptionPassword = true
+
+if subscriptionEmail and subscriptionPassword == true then
+    print("Login Secessful!")
+    print("Make sure to check the subscriptionFeatures folder to get exusive subscription features!")
+end
