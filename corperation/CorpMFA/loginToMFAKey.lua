926a80971f9cffd5ba8939483991217ef5dfb132
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-27 16:03:55 -0600

    ic

---
+++ corperation/CorpMFA/loginToMFAKey.lua
@@ -0,0 +1,13 @@
+file = io.open("CORP_MFA.txt", "r")
+
+storedCode = file:read("*all")
+file:close()
+
+print("Enter your MFA code: ")
+local inputedMFACode = io.read()
+
+if mfaInputCode == storedMFA then
+    print("MFA code is correct!")
+else
+    print("Incorrect MFA code, please try again.")
+end
