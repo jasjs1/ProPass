a54c4014dbf82034c81ec148af777e68de51782a
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-28 07:28:28 -0600

    may not be working yet....

---
+++ import/import.lua
@@ -0,0 +1,57 @@
+io.write("Password: ")
+local importPassword = io.read()
+
+io.write("Password for: ")
+local passwordFor = io.read()
+
+-- Write data to file
+
+local file = io.open("importedPasswords.txt", "a")
+
+file:write("Password: " .. password .. "\n")
+file:write("Password for: " .. passwordFor .. "\n")
+file:write("" .. "\n")
+
+io.write("Password: ")
+local importPassword = io.read()
+
+io.write("Password for: ")
+local passwordFor = io.read()
+
+-- Write data to file
+
+local file = io.open("importedPasswords.txt", "a")
+
+file:write("Password: " .. password .. "\n")
+file:write("Password for: " .. passwordFor .. "\n")
+file:write("" .. "\n")
+
+io.write("Password: ")
+local importPassword = io.read()
+
+io.write("Password for: ")
+local passwordFor = io.read()
+
+-- Write data to file
+
+local file = io.open("importedPasswords.txt", "a")
+
+file:write("Password: " .. password .. "\n")
+file:write("Password for: " .. passwordFor .. "\n")
+file:write("" .. "\n")
+
+io.write("Password: ")
+local importPassword = io.read()
+
+io.write("Password for: ")
+local passwordFor = io.read()
+
+-- Write data to file
+
+local file = io.open("importedPasswords.txt", "a")
+
+file:write("Password: " .. password .. "\n")
+file:write("Password for: " .. passwordFor .. "\n")
+file:write("" .. "\n")
+
+end
