addf8a5c3b087e5c085d1c554e3a2a9c7d918170
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-02 07:36:53 -0600

    changed recovery code file

--- corperation/recoveryCode/createRecoveryCode.lua
+++ corperation/recoveryCode/createRecoveryCode.lua
@@ -1,9 +1,9 @@
 io.write("Corperation Recovery Code: ")
 local corperationRecoveryCode = io.read()
 
-local file = io.open("CORP_recoveryCode.txt", "w")
+local file = io.open("RecoveryCode_CORP.txt", "w")
 
 file:write(corperationRecoveryCode)
 file:close()
 
-print("Corperation Recovery Code file was created. Check the explorer for: CORP_recoveryCode.txt")
+print("Corperation Recovery Code file was created. Check the explorer for: RecoveryCode_CORP.txt")
