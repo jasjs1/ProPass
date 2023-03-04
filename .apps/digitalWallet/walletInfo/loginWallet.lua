f63b4cf0b5df94952cde4c761aaac9026a83643b
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-04 15:23:41 -0600

    initial commit

---
+++ .apps/digitalWallet/walletInfo/loginWallet.lua
@@ -0,0 +1,16 @@
+local function login()
+
+    io.write("Email: ")
+    local email = io.read()
+
+    io.write("Password: ")
+    local password = io.read()
+
+    io.write("2FA code: ")
+    local wallet2FACode = io.read()
+    
+    print("Login secessfull!")
+
+
+end
+login()
