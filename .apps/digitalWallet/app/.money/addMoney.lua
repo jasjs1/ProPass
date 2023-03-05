02bd34b045d5a9c7c5a06e35cb3600fe43cdfa7c
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-05 07:33:33 -0600

    added audit trail

--- .apps/digitalWallet/app/.money/addMoney.lua
+++ .apps/digitalWallet/app/.money/addMoney.lua
@@ -3,5 +3,8 @@ local totalMoney = io.read()
 
 local file = io.open("totalMoney.txt", "w")
 
-file:write(totalMoney)
+file:write(totalMoney .. "\n")
+file:write("Audit date" .. os.date("%M_%d_%y_%H_%M_%S") .. "\n")
 file:close()
+
+print("Total remaning money: " .. "$" .. totalMoney)
