e5eb01d404f87e7dcba4932f09a2e4ab029c1bf0
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-05 08:05:20 -0600

    added require subscription

--- .apps/digitalWallet/app/.money/addMoney.lua
+++ .apps/digitalWallet/app/.money/addMoney.lua
@@ -2,7 +2,16 @@ io.write("Total money: $")
 local totalMoney = io.read()
 
 local file = io.open("totalMoney.txt", "w")
+io.write("Total money: $")
+local totalMoney = io.read()
 
+local file = io.open("totalMoney.txt", "w")
+
+file:write(totalMoney .. "\n")
+file:write("Audit date" .. os.date("%M_%d_%y_%H_%M_%S") .. "\n")
+file:close()
+
+print("Total remaning money: " .. "$" .. totalMoney)
 file:write(totalMoney .. "\n")
 file:write("Audit date" .. os.date("%M_%d_%y_%H_%M_%S") .. "\n")
 file:close()
