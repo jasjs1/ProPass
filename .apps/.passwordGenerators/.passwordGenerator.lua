5c5a0fc0580370738b5563c496b35d9d0f04fa30
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-05 15:19:10 -0600

    added func inside of a func

--- .apps/.passwordGenerators/.passwordGenerator.lua
+++ .apps/.passwordGenerators/.passwordGenerator.lua
@@ -8,60 +8,7 @@ local function generateANewPassword(length)
     return password
 end
 
-local function writeDataToFile(password, passwordWebHost)
-    local file = io.open("savedPassword.txt", "a")
-    file:write("Generated saved password: " .. password .. "\n")
-    file:write("Place that password is used: " .. passwordWebHost .. "\n")
-    file:write("\n")
-    file:close()
-end
 
-local function generatePassword()
-    print("") -- Just a separator separating the above contents from the Visual Studio Code extension generated text
-    io.write("Enter desired password length (between 8 and 60): ")
-    local length = tonumber(io.read())
-
-    if not length or length < 8 or length > 60 then
-        print("Invalid password length. Please enter a number between 8 and 60 characters.")
-        return 
-    end
-
-    io.write("Where will the password be used: ")
-    local passwordWebHost = io.read()
-
-    local password = generateANewPassword(length)
-
-    print("If you would like to generate a new password. Type 'new'.")
-    print("Generated password: " .. password)
-
-    local input = io.read()
-
-    if input == "new" then
-        password = generateANewPassword(length)
-        print("New password: " .. password)
-    end
-
-    writeDataToFile(password, passwordWebHost)
-end
-generatePassword()
-
-local function generateANewPassword(length)
-    local charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-+!@#$%^&*();"
-    local password = ""
-    for i = 1, length do
-        local index = math.random(1, string.len(charset))
-        password = password .. string.sub(charset, index, index)
-    end
-    return password
-end
-
-local function writeDataToFile(password, passwordWebHost)
-    local file = io.open("savedPassword.txt", "a")
-    file:write("Generated saved password: " .. password .. "\n")
-    file:write("Place that password is used: " .. passwordWebHost .. "\n")
-    file:write("\n")
-    file:close()
-end
 
 local function getNewPass()
     print("") -- Just a separator separating the above contents from the Visual Studio Code extension generated text
@@ -83,18 +30,23 @@ local function getNewPass()
 
     local input = io.read()
 
-    if input == "new" then
+    while input == "new" do
         password = generateANewPassword(length)
+        print("Where will the password be used: ")
+        local passwordWebHost = io.read()
         print("New password: " .. password)
+        writeDataToFile(password, passwordWebHost)
+        input = io.read()
     end
 
+    local function writeDataToFile(password, passwordWebHost)
+        local file = io.open("savedPassword.txt", "a")
+        file:write("Generated saved password: " .. password .. "\n")
+        file:write("Place that password is used: " .. passwordWebHost .. "\n")
+        file:write("\n")
+        file:close()
+    end
     writeDataToFile(password, passwordWebHost)
 end
 
-while true do
-    local input = io.read()
-    if input == "new" then
-    getNewPass()
-    end
-end
-
+getNewPass()
