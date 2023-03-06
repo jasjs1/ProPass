40def775704edfb3f8384f845fb3538790dacbfb
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-06 07:06:39 -0600

    added TLD for signup

--- premium/getPreimum.lua
+++ premium/getPreimum.lua
@@ -1,25 +1,45 @@
+local function validateInputtedFields(name, email, signupPassword, paymentType)
+    print("\nValidate that the information below is correct:")
+    print("\nName: " .. name)
+    print("Email: " .. email)
+    print("Password: " ..signupPassword)
+    print("Payment type: " ..paymentType)
+end
+
 local function createSubscription(name, email, signupPassword, paymentType)
-
     local file = io.open("subscriptionInfo.txt", "w")
-
     file:write("Name: " .. name .. "\n")
     file:write("Email: " .. email .. "\n")
     file:write("Password: " .. signupPassword .. "\n")
     file:write("Payment type: " .. paymentType .. "\n")
-
     file:close()
-
     print("Subscription created successfully!")
+end
 
-    end
-    
 local function signUpForSubscription()
-
     print("Name:")
     local name = io.read()
 
-    print("Email:")
-    local subscriptionEmail = io.read()
+    repeat
+        print("Email:")
+        local subscriptionEmail = io.read()
+
+        if subscriptionEmail:match(".+@.+%.com$") or subscriptionEmail:match(".+@.+%.org$") 
+        or subscriptionEmail:match(".+@.+%.edu$")
+        or subscriptionEmail:match(".+@.+%.net$")
+        or subscriptionEmail:match(".+@.+%.io$")
+        or subscriptionEmail:match(".+@.+%.me$")
+        or subscriptionEmail:match(".+@.+%.ca$")
+        or subscriptionEmail:match(".+@.+%.tech$")
+        or subscriptionEmail:match(".+@.+%.ai$")
+        or subscriptionEmail:match(".+@.+%.studio$")
+        or subscriptionEmail:match(".+@.+%.gov$")
+        then
+            break
+        else
+            print("Invalid email address. Please use a proper TLD, if you don't know what a TLD is, please check tld.md")
+        end
+    until false
 
     print("Password:")
     local subscriptionPassword = io.read()
@@ -27,28 +47,15 @@ local function signUpForSubscription()
     print("Payment type:")
     local paymentType = io.read()
 
-    return name, email, signupPassword, paymentType
+    return name, subscriptionEmail, subscriptionPassword, paymentType
 end
-    
-    local function validateInputtedFields(name, email, signupPassword, paymentType)
 
-    print("\nValidate that the information below is correct:")
+local name, subscriptionEmail, subscriptionPassword, paymentType = signUpForSubscription()
+validateInputtedFields(name, subscriptionEmail, subscriptionPassword, paymentType)
+print("\nType 'yes' with the information above to create a subscription.")
 
-    print("\nName: " .. name)
-    print("Email: " .. email)
-    print("Password: " ..signupPassword)
-    print("Payment type: " ..paymentType)
-end
-    
-    local name, email, signupPassword, paymentType = signUpForSubscription()
-    
-    validateInputtedFields(name, email, signupPassword, paymentType)
-    print("\nType 'yes' with the information above to create a subscription.")
-    
-    if io.read() == "yes" then
-
-    createSubscription(name, email, signupPassword, paymentType)
-
-    else
+if io.read() == "yes" then
+    createSubscription(name, subscriptionEmail, subscriptionPassword, paymentType)
+else
     print("Subscription not created.")
 end
