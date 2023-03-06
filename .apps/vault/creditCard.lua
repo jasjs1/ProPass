0dec4fe3a7034d6f0a2479ed864dae5d1130eaae
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-06 16:01:19 -0600

    added in new repo

---
+++ .apps/vault/creditCard.lua
@@ -0,0 +1,22 @@
+io.write("Credit card number: ")
+local creditCardNumber = io.read()
+
+io.write("Number on back of card ")
+local ccBackNumber = io.read()
+
+io.write("EXP date: ")
+local expDate = io.read()
+
+local function write()
+
+    local file = io.open("CreditCardInformation.txt", "w")
+
+    file:write(creditCardNumber, ", ")
+    file:write(ccBackNumber, ", ")
+    file:write(expDate, ", ")
+
+    file:close()
+
+    print("File has been created. Check the explorer!")
+end 
+write()
