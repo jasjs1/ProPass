96dfc15522134140393bd571b60cba860676caa8
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-07 18:21:46 -0600

    bug fixed // added audit date

--- corperation/cardientialStorage/.passwordStore.lua
+++ corperation/cardientialStorage/.passwordStore.lua
@@ -1,7 +1,8 @@
 file = io.open("passwordStore.txt", "a")
 
-io.output():write("Password: ")
-inputStore = io.input()file:read()
+io.write("Password for store: ")
+inputStore = io.read()
 
-file:write(inputStore)
+file:write(inputStore .. "\n")
+file:write("Date audited: " .. os.date("%M_%d_%y_%H_%m") .. "\n")
 file:close()
