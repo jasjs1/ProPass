b265d2bbf5fc593a8203babaf9bd293d23afcb2e
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-09 15:58:03 -0600

    added date added // deleted asking the user what date that the password is created

--- corperation/shareAPassword/basicShare.lua
+++ corperation/shareAPassword/basicShare.lua
@@ -1,9 +1,6 @@
 
 print("")
 
-io.write("Today's date: ")
-local todaysDate = io.read()
-
 io.write("Password to share: ")
 local passwordToShare = io.read()
 
@@ -17,7 +14,6 @@ local function validateSharedPasswordInformation()
 
     print("Validate that the information that you have provided below is correct.")
 
-    print("Today's date: " .. todaysDate)
     print("Password to share: " .. passwordToShare)
     print("Web host: " .. webHostForPassword)
     print("Recipiants: " .. recipiants)
@@ -29,7 +25,8 @@ local function writeDataToFile()
 
     local file = io.open("CORP_sharedPassword.txt", "a")
 
-    file:write("Date: " .. todaysDate .. "\n")
+    -- file:write("Date: " .. todaysDate .. "\n")
+    file:write("Date created: " .. os.date("%M_%d_%y_%H_%m"))
     file:write("Password to share: " .. passwordToShare .. "\n")
     file:write("Website host: " .. webHostForPassword .. "\n")
     file:write("Recipiants: " .. recipiants  .. "\n")
