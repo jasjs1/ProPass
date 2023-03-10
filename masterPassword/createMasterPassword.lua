05a4044d122d6f48fd9dbf86555f5abcc3090497
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-09 19:50:59 -0600

    added hour and minuite of date created

--- masterPassword/createMasterPassword.lua
+++ masterPassword/createMasterPassword.lua
@@ -5,7 +5,7 @@ local function writeDataToFile()
     local masterPasswordFile = io.open("MasterPassword.txt", "w")
     
     masterPasswordFile:write("Master Password: " .. masterPassword .. "\n")
-    masterPasswordFile:write("Date Created: " .. os.date("%m_%d_%y"))
+    masterPasswordFile:write("Date Created: " .. os.date("%m_%d_%y_%H_%m"))
     
     masterPasswordFile:close()
 
