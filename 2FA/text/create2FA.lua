8a2644ce0a069eb09f0d0a0e9518943285bba7b1
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-10 11:54:53 -0600

    Delete 2FA/text directory

--- 2FA/text/create2FA.lua
+++
@@ -1,9 +0,0 @@
-file = io.open("2FactorCode.txt", "w")
-
-io.output():write("Your 2FA code: ")
-twoFactorCode = io.input():read()
-
-
-file:write(twoFactorCode)
-file:close()
-print("2FA was created.")
