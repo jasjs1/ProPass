cc79fabbf4184f386b4f47d5c8c96b51d0fa1576
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-11 14:40:50 -0600

    initial commit

---
+++ premiumFeatures/vault+/shared/emailShared.lua
@@ -0,0 +1,71 @@
+local accountUsername
+local webURL
+local password
+local recipientEmail
+
+local function storeAndWrite()
+    io.write("Account username: ")
+    accountUsername = io.read()
+
+    io.write("Web URL: ")
+    webURL = io.read()
+
+    io.write("Password for account: ")
+    password = io.read()
+
+    io.write("Recipient email: ")
+    recipientEmail = io.read()
+
+    print("") -- spacer
+    print("") -- spacer
+
+    print("Validate that the information below is correct: ")
+    print("")
+
+    print("Account username: " .. accountUsername)
+    print("Web URL: " .. webURL)
+    print("Password for account: " .. password)
+    print("Recipient email: " .. recipientEmail)
+
+    print("")
+    print("Type 'yes' if the information above is correct.")
+
+    local input = io.read()
+
+    if input == "yes" then
+        local file = io.open("email_vault-shared.txt", "a")
+
+        file:write("Account username: " .. accountUsername .. "\n")
+        file:write("Web URL: " .. webURL .. "\n")
+        file:write("Password for account: " .. password .. "\n")
+        file:write("Recipient email: " .. recipientEmail .. "\n")
+
+        file:close()
+        return true
+    end
+    return false
+end
+
+local subscriptionFile = io.open("subscriptionInfo.txt")
+
+if subscriptionFile == nil then
+    print("You need a ProPass Premium subscription to use Vault+. Please create a subscription to use this.")
+    print("Type '!help' to get more information.")
+
+    local input = io.read()
+
+    if input == "!help" then
+        print("")
+        print("Folder path for Premium: premium/getPremium.lua")
+        print("")
+        return
+    end
+else
+    subscriptionFile:close()
+    local success = storeAndWrite()
+    if success then
+        print("Shared vault created. Check the explorer for: email_vault-shared.txt")
+    else
+        print("Information not stored.")
+    end
+end
