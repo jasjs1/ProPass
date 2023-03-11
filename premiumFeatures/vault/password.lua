e0ce4cbd147ff6ab322a82c389fa412042299118
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-11 08:29:09 -0600

    added prompt user to type "yes" to write file // format change
    Also:
    Added msg that the file has been created and check the file explorer for: file name

--- premiumFeatures/vault/password.lua
+++ premiumFeatures/vault/password.lua
@@ -5,6 +5,9 @@ local doesPasswordExpire
 local otherNotes
 
 local function storePassword()
+
+    print("") -- Spacer
+
     io.write("Password for vaulting: ")
     passwordForVaulting = io.read()
 
@@ -27,12 +30,22 @@ storePassword()
 local function validateInformationAndWriteToFile()
     print("")
     print("")
-    print("Validate that the information below is correct.")
+
+    print("Validate that the info below is correct.")
+
+    print("")
 
     print("Password: " .. passwordForVaulting)
     print("Is password shared: " .. isPasswordShared)
     print("Does password expire: " .. doesPasswordExpire)
     print("Other notes: " .. otherNotes)
+
+    print("")
+    print("Type 'yes' if the info above is correct")
+
+    local input = io.read()
+
+    if input == "yes" then
 
     -- Write data to file
     local file = io.open("passwordVault-prem.txt", "a")
@@ -45,6 +58,10 @@ local function validateInformationAndWriteToFile()
     file:write("\n")
 
     file:close()
+
+    print("File created! Check the explorer for: passwordVault-prem.txt")
+
+    end
 end
 
 validateInformationAndWriteToFile()
