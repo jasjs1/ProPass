ca244a0017d4f953f69b66e5a5ece707c8875dcc
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-11 18:34:13 -0600

    require master password

--- .apps/passwordSharing.lua
+++ .apps/passwordSharing.lua
@@ -1,109 +1,65 @@
 --[[
-    This is the basic password sharing. If you would like to get extra fields to share. That is a subscription.
+    This is the basic password sharing. If you would like to get extra fields to share, that is a subscription.
 ]]
 
 local function combineFuncs()
-    
-    -- continue with the rest of your code here
-    
+    local email
     repeat
         io.write("Email: ")
-        local email = io.read()
-      
+        email = io.read()
+
         -- Check if the email matches the pattern
-        if email:match(".+@.+%.com$") or email:match(".+@.+%.org$") 
-        or email:match(".+@.+%.edu$")
-        or email:match(".+@.+%.net$")
-        or email:match(".+@.+%.io$")
-        or email:match(".+@.+%.me$")
-        or email:match(".+@.+%.ca$")
-        or email:match(".+@.+%.tech$")
-        or email:match(".+@.+%.ai$")
-        or email:match(".+@.+%.studio$")
-        or email:match(".+@.+%.gov$")
-        then
-          -- Valid email address
-          break
+        if email:match(".+@.+%.com$") or email:match(".+@.+%.org$") or
+            email:match(".+@.+%.edu$") or email:match(".+@.+%.net$") or
+            email:match(".+@.+%.io$") or email:match(".+@.+%.me$") or
+            email:match(".+@.+%.ca$") or email:match(".+@.+%.tech$") or
+            email:match(".+@.+%.ai$") or email:match(".+@.+%.studio$") or
+            email:match(".+@.+%.gov$") then
+            -- Valid email address
+            break
         else
-          -- Invalid email address
-                print("Invalid email address. Please enter a valid email address.")
+            -- Invalid email address
+            print("Invalid email address. Please enter a valid email address.")
         end
-      until false
+    until false
+
     io.write("Password: ")
     local sharedPassword = io.read()
 
     io.write("Recipients email: ")
     local recipientsEmail = io.read()
 
-    local file = io.open("sharedPassword.txt", "w")
+    local file, err = io.open("sharedPassword.txt", "w")
+    if not file then
+        print("Error opening file:", err)
+        return
+    end
+
     file:write("Email/username: " .. email .. "\n")
     file:write("Password: " .. sharedPassword .. "\n")
     file:write("Recipients email: " .. recipientsEmail .. "\n")
+
     file:close()
 
+    print("Password shared successfully!")
 end
 
-local function getUserInput()
-
-    local email = ""
-    while not email:match(".+@.+%..+") do
-      io.write("Email: ")
-      email = io.read()
-      if not email:match(".+@.+%..+") then
-        print("Invalid email address. Please enter a valid email address.")
-      end
-    end
-
-    repeat
-        print("Password to share (8-100 characters):")
-        local sharedPassword = io.read()
-
-        if #sharedPassword < 8 or #sharedPassword > 100 then
-            print("Password must be between 8 to 100 characters.")
-        else
-            break
-        end
-    until false
-
-    io.write("Recipients email: ")
-    local recipientsEmail = io.read()
-
-
-    io.write("What site is the password used on: ")
-    local passwordWebHost = io.read()
-
-    return email, sharedPassword, recipientsEmail, passwordWebHost
-end
-
-local email, sharedPassword, recipientsEmail, passwordWebHost = getUserInput()
-local function writeToFile(email, sharedPassword, recipientsEmail, passwordWebHost)
-
-    local file = io.open("sharedPassword.txt", "w")
-    file:write("Email/username: " .. email .. "\n")
-    file:write("Password: " .. sharedPassword .. "\n")
-    file:write("Recipients email: " .. recipientsEmail .. "\n")
-    file:write("Password is used on: " .. passwordWebHost .. "\n")
-    file:close()
-
-    print("Shared password created!")
-
-end
-writeToFile(email, sharedPassword, recipientsEmail, passwordWebHost)
-
-while true do 
-
-    print("")
-    print("Type 'new' to create a new shared password")
-    print("")
-
-    
-    local input = io.read()
-
-    if input == "new" then
-        combineFuncs()
-
-    elseif input == "" then
-        print("Invalid input")
-        -- break
-    end
-end
+-- Function to check if the entered password matches the MasterPassword
+function checkPassword(password)
+    local file = io.open("MasterPassword.txt", "r") -- open the file for reading
+    local contents = file:read("*all") -- read the contents of the file
+    file:close() -- close the file
+  
+    return (password == contents) -- return true if the passwords match, false otherwise
+  end
+  
+  -- Get user input for password
+  io.write("Enter the master password:")
+  local input = io.read()
+  
+  if checkPassword(input) then
+    combineFunc()
+  else
+    print("Passwords do not match. Exiting...")
+  end
+  
